e98e0ffd6f515b9cfe0554e3d40d87c5fcd07c3e
Haziq Kamel <haziq@alphapod.com>
2026-02-27 12:57:33 +0800

    feat: add gray and info colors to AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,6 +32,8 @@ class AdminPanelProvider extends PanelProvider
             ->darkModeBrandLogo('/images/logo-dark.png')
             ->colors([
                 'primary' => Color::convertToOklch('#7716B9'),
+                'gray'    => Color::Slate,
+                'info'    => Color::convertToOklch('#1E40AF'),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
