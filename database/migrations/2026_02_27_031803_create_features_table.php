e735907297f0323b57a1baf6e3361181c2f91682
Haziq Kamel <haziq@alphapod.com>
2026-02-27 11:39:03 +0800

    feat: create migration for features table

---
+++ database/migrations/2026_02_27_031803_create_features_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('features', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedSmallInteger('status')->default(0);
+            $table->unsignedSmallInteger('type')->default(0);
+            $table->text('description')->nullable();
+            $table->smallInteger('effort_in_days')->unsigned()->default(0);
+            $table->smallInteger('priority')->unsigned()->default(0);
+            $table->decimal('cost', 10, 2)->unsigned()->default(0.00);
+            $table->dateTime('target_delivery_date')->nullable();
+            $table->dateTime('delivered_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('features');
+    }
+};
