5fc0017b4fd1c6474e39f7f4fe659617923d859a
Haziq Kamel <haziq@alphapod.com>
2026-02-27 17:25:43 +0800

    feat: enhance FilamentUiServiceProvider with table and column configurations for improved UI interactions

--- app/Providers/FilamentUiServiceProvider.php
+++ app/Providers/FilamentUiServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Filament\Actions\CreateAction;
+use Filament\Tables\Columns\Column;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
 use Illuminate\Support\ServiceProvider;
 
 class FilamentUiServiceProvider extends ServiceProvider
@@ -19,7 +22,7 @@ class FilamentUiServiceProvider extends ServiceProvider
      * Bootstrap services.
      */
     public function boot(): void
-    {   
+    {
         /**
          * Configure the CreateAction to uppercase the first letter of the model label in its default label.
          */
@@ -29,6 +32,30 @@ class FilamentUiServiceProvider extends ServiceProvider
                     key: 'filament-actions::create.single.label',
                     replace: ['label' => ucwords($action->getModelLabel())]
                 ));
+        });
+
+        /**
+         * Configure the Table to be reorderable and the Column to be toggleable by default.
+         */
+        Table::configureUsing(function (Table $table) {
+            $table
+                ->reorderableColumns();
+        });
+
+        /**
+         * Configure the Column to be toggleable by default.
+         */
+        Column::configureUsing(function (Column $column) {
+            $column
+                ->toggleable();
+        });
+
+        /**
+         * Configure the TextColumn to be sortable by default.
+         */
+        TextColumn::configureUsing(function (TextColumn $column) {
+            $column
+                ->sortable();
         });
     }
 }
