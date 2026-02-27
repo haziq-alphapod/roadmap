c7a3ea9922de76084edb8669e4934490d44b6393
Haziq Kamel <haziq@alphapod.com>
2026-02-27 14:30:28 +0800

    feat: add password reset support to AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->unsavedChangesAlerts()
             ->databaseTransactions()
             ->registration()
+            ->passwordReset()
             ->brandLogo('/images/logo-light.png')
             ->darkModeBrandLogo('/images/logo-dark.png')
             ->colors([
